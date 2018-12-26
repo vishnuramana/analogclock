50f25763348a6e50205eed15f1e5b3d2820304ac
Vishnu Ramana <vishnu_dew@yahoo.com>
2018-12-26 17:23:52 +0530

    Initial commit for gh-pages

---
+++ precache-manifest.62f165e24c0fd4a253c3ab31015124dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7686b94ac57ec470bde",
+    "url": "/analogclock/static/css/main.ef6d065c.chunk.css"
+  },
+  {
+    "revision": "a7686b94ac57ec470bde",
+    "url": "/analogclock/static/js/main.a7686b94.chunk.js"
+  },
+  {
+    "revision": "77958300264c6869a197",
+    "url": "/analogclock/static/js/1.77958300.chunk.js"
+  },
+  {
+    "revision": "d29e5bddad407091052e",
+    "url": "/analogclock/static/js/runtime~main.d29e5bdd.js"
+  },
+  {
+    "revision": "f8868f7ecd5ed68a1bb4cc648e500e36",
+    "url": "/analogclock/index.html"
+  }
+];
