188bde321d033b70166de3f85df2f91f7eb2f3e4
Vishnu Ramana <vishnu_dew@yahoo.com>
2019-01-05 22:20:23 +0530

    Initial commit of npm package

--- src/index.js
+++ src/index.js
@@ -1,6 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React, { Component } from 'react';
+import { ClockContainer, ClockBaseBorder, ClockBase, ClockCenter } from './ClockComponents';
+import Hand from './Hand.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class AnalogClock extends Component {
+
+    constructor(props) {
+        super(props);
+        let date = new Date();
+        this.state = {
+            seconds: date.getSeconds(),
+            minutes: date.getMinutes(),
+            hours: date.getHours() > 12 ? date.getHours() - 12 : date.getHours()
+        }
+    }
+
+    componentDidMount() {
+        setInterval(() => {
+            let date = new Date();
+            this.setState({
+                seconds: date.getSeconds(),
+                minutes: date.getMinutes(),
+                hours: date.getHours() > 12 ? date.getHours() - 12 : date.getHours()
+            })
+        }, 1000)
+    }
+
+    render() {
+        const { width, border, borderColor, baseColor } = this.props;
+        return (
+            <ClockContainer width={width}>
+                <ClockBaseBorder border={border} borderColor={borderColor}>
+                    <ClockBase baseColor={baseColor}>
+                        <ClockCenter />
+                        <Hand type="second" {...this.state} />
+                        <Hand type="minute" {...this.state} />
+                        <Hand type="hour" {...this.state} />
+                    </ClockBase>
+                </ClockBaseBorder>
+            </ClockContainer>
+
+        )
+    }
+}
+
+export default AnalogClock;
