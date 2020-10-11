d4fe8df3045f80d59e3899e7e6174de3881f891d
Vishnu Ramana <vishnu_dew@yahoo.com>
2020-10-11 17:06:54 +0530

    Updates

--- precache-manifest.a110ad833e40288e8d4efaa73f4438c8.js
+++ precache-manifest.a110ad833e40288e8d4efaa73f4438c8.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "00f8503e9326fdf40c1f",
-    "url": "/analogclock/static/css/main.0fe835e8.chunk.css"
+    "revision": "228da9f7302915a027c4",
+    "url": "/analogclock/static/css/main.4e102394.chunk.css"
   },
   {
-    "revision": "00f8503e9326fdf40c1f",
-    "url": "/analogclock/static/js/main.00f8503e.chunk.js"
+    "revision": "228da9f7302915a027c4",
+    "url": "/analogclock/static/js/main.228da9f7.chunk.js"
   },
   {
     "revision": "235cc238be2e0cd99ab2",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/analogclock/static/js/runtime~main.d29e5bdd.js"
   },
   {
-    "revision": "202645ae8ab33eb33bfb9a929c32f2d1",
+    "revision": "3636d59e17f7e74288ac06d51aa9cdc4",
     "url": "/analogclock/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/analogclock/precache-manifest.c9361f79513973e804c622862bfb39d8.js"
+  "/analogclock/precache-manifest.a110ad833e40288e8d4efaa73f4438c8.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/analogclock/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
